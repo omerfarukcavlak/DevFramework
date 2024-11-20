4c1dc072cd68386b88d0065bc9c5b889e5075da3
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-20 22:28:34 +0300

    NHibernate için birim testleri yazıldı

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -15,7 +15,7 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
     {
         protected override ISessionFactory InitializeFactory()
         {
-            return Fluently.Configure().Database(MsSqlCeConfiguration.MsSqlCe40.ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext"))).Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012.ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext"))).Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
         }
     }
 }
