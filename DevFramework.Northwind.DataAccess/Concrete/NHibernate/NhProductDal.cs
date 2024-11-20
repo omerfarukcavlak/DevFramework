64b59a68f62985edef8c8a36a18ce48651604053
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-21 00:20:07 +0300

    (NH) Complex Tip Eklendi

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -12,13 +12,25 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
     {
+        private NHibernateHelper _nHibernateHelper;
         public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
+            _nHibernateHelper = nHibernateHelper;
         }
 
         public List<ProductDetail> GetProductDetails()
         {
-            throw new NotImplementedException();
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>() on p.CategoryId equals c.CategoryId select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 CategoryName = c.CategoryName,
+                                 ProductName = p.ProductName
+                             };
+                return result.ToList();
+            }
         }
     }
 }
