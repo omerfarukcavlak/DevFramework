8981f97c693429703917a5a65e7f2bff98a0c314
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-21 01:00:03 +0300

    İş Katmanı İmplementasyonları Yapıldı

---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,53 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Silme Gerçekleşemedi");
+            }
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+    }
+}
