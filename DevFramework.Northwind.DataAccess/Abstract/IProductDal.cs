30d98994a074d06bd25c96ffee226fefc746f65f
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-21 00:14:57 +0300

    (EF) Complex Tip Eklendi

--- DevFramework.Northwind.DataAccess/Abstract/IProductDal.cs
+++ DevFramework.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
 ﻿using DevFramework.Core.DataAccess;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,6 @@ namespace DevFramework.Northwind.DataAccess.Abstract
     public interface IProductDal:IEntityRepository<Product>
     {
        
-
+        List<ProductDetail> GetProductDetails();
     }
 }
