0ffb375b8e7d37df6fef42786af1007bc9e52d58
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-20 22:13:44 +0300

    NHibernate için SqlServerHelper, Mapping İmplementasyonları Yapıldı

---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,21 @@
+﻿using DevFramework.Core.DataAccess.NHihabernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHelper : NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlCeConfiguration.MsSqlCe40.ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext"))).Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+        }
+    }
+}
