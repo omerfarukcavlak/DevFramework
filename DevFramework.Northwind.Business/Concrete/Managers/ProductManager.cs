bbcff5efbdea1f292e6adb25f48c4abd7e9a5811
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-21 02:06:26 +0300

    FluentValidation,Postsharp kullanılarak Aspect yazıldı

--- DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -1,6 +1,10 @@
-﻿using DevFramework.Northwind.Business.Abstract;
+﻿using DevFramework.Core.Aspects.Postsharp;
+using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
 using DevFramework.Northwind.DataAccess.Abstract;
 using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +16,13 @@ namespace DevFramework.Northwind.Business.Concrete.Managers
     public class ProductManager : IProductService
     {
         private IProductDal _productDal;
+
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
         }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
         public Product Add(Product product)
         {
             return _productDal.Add(product);
@@ -45,8 +52,10 @@ namespace DevFramework.Northwind.Business.Concrete.Managers
             return _productDal.Get(p => p.ProductId == id);
         }
 
+        [FluentValidationAspect(typeof(ProductValidator))]
         public Product Update(Product product)
         {
+            
             return _productDal.Update(product);
         }
     }
